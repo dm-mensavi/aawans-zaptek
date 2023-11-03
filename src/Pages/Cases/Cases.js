e3f67cff4c78183668ed2c7ae16a249ae7e2e9c3
dm-mensavi <mensavidavid@gmail.com>
2023-11-03 15:25:19 +0000

    resize image width

--- src/Pages/Cases/Cases.js
+++ src/Pages/Cases/Cases.js
@@ -18,11 +18,14 @@ function Cases() {
 					{caseData.map((item, index) => (
 						<Link key={index} to={`/cases/${item.id}`}  className="relative">
 							<img
+                // width={400}
+                // height= {400}
+                
 								src={require(`../../public/${item.image}.jpg`)}
 								alt={item.title}
-								className="rounded-2xl w-80 h-80"
+								className="rounded-2xl w-96 h-80 object-center"
 							/>
-							<div className="absolute top-0 left-0 w-80 flex flex-col justify-self-end opacity-0 rounded-xl align-bottom text-center transition-opacity duration-300 hover:opacity-100 inset-0 bg-gradient-to-t from-gray-900 ">
+							<div className="absolute top-0 left-0 w-96 flex flex-col justify-self-end opacity-0 rounded-xl align-bottom text-center transition-opacity duration-300 hover:opacity-100 inset-0 bg-gradient-to-t from-gray-900 ">
 								<p className="text-white text-3xl pt-52">{item.title} </p>
 								<p className="text-2xl text-primary">{item.subtitle} </p>
 							</div>
