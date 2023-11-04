cf14be17ae949643df6d9d66b65b129892e08c20
dm-mensavi <mensavidavid@gmail.com>
2023-11-04 00:39:28 +0000

    Add icons to practice page

--- src/Pages/PracticeAreas/PracticeAreaData.js
+++ src/Pages/PracticeAreas/PracticeAreaData.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { ReactComponent as Icon1 } from '../../public/icon-1.svg';
-import { ReactComponent as Icon2 } from '../../public/icon-2.svg';
-import { ReactComponent as Icon3 } from '../../public/icon-3.svg';
-import { ReactComponent as Icon4 } from '../../public/icon-4.svg';
-import { ReactComponent as Icon5 } from '../../public/icon-5.svg';
-import { ReactComponent as Icon6 } from '../../public/icon-6.svg';
-
+import React from "react";
+import Icon1 from "../../public/practice-icon-1.png";
+import Icon2 from "../../public/practice-icon-2.png";
+import Icon3 from "../../public/practice-icon-3.png";
+import Icon4 from "../../public/practice-icon-4.png";
+import Icon5 from "../../public/practice-icon-5.png";
+import Icon6 from "../../public/practice-icon-6.png";
 
 export const practiceAreaData = [
-  { id: 1, title: "Health Care Law", image: "practice-img-1", icon: <Icon1 /> },
-  { id: 2, title: "Real Estate Law", image: "practice-img-2", icon:<Icon2 /> },
-  { id: 3, title: "Corporate Law", image: "practice-img-3", icon: <Icon3 />},
-  { id: 4, title: "Traffic Violence", image: "practice-img-4", icon: <Icon4 /> },
-  { id: 5, title: "Drug Offenses", image: "practice-img-5", icon: <Icon5 /> },
-  { id: 6, title: "Domestic Violence", image: "practice-img-6", icon:<Icon6 /> },
-]
-
-
+	{ id: 1, title: "Health Care Law", image: "practice-img-1", icon: Icon1 },
+	{ id: 2, title: "Real Estate Law", image: "practice-img-2", icon: Icon2 },
+	{ id: 3, title: "Corporate Law", image: "practice-img-3", icon: Icon3 },
+	{ id: 4, title: "Traffic Violence", image: "practice-img-4", icon: Icon4 },
+	{ id: 5, title: "Drug Offenses", image: "practice-img-5", icon: Icon5 },
+	{ id: 6, title: "Domestic Violence", image: "practice-img-6", icon: Icon6 },
+];
