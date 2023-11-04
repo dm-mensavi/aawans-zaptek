be854daac36b123235f407e13dfca577f902dcd6
Kobby7777 <godfredmirekuowusu@gmail.com>
2023-11-04 04:10:44 +0000

    Done with the blog categories

--- src/Pages/Blog/BlogDetails.js
+++ src/Pages/Blog/BlogDetails.js
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
 import getDataById from "../../GetDataById";
-import { BlogData } from "./BlogData";
+// import { BlogData } from "./BlogData";
 import Header from "../../components/Header";
 import { Helmet } from "react-helmet";
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import BlogInfo from './BlogInfo'
+import {data} from './Data'
 
 function BlogDetails() {
 	//Scroll to the top of the page on load
@@ -16,7 +18,13 @@ function BlogDetails() {
 	// get the page id from the route
 	let { id } = useParams();
 
-	const blogItem = getDataById(BlogData, id);
+	// const blogItem = getDataById(BlogData, id);
+	const blogItem = getDataById(data, id);
+
+	const items = data.map((item) =>
+
+		<BlogInfo title={item.title} picture={item.picture}/>
+	)
 
 	return (
 		<div>
@@ -26,7 +34,7 @@ function BlogDetails() {
 			<Header pageTitle="Blog Details" name="Blog" />
 
 			{/* Your content and design goes here */}
-			<div className="flex flex-col items-center my-20">
+			{/* <div className="flex flex-col items-center my-20">
 				<div className="h3 text-center">{blogItem.title}</div>
 				<img
 					width={700}
@@ -34,7 +42,10 @@ function BlogDetails() {
 					src={require(`../../public/${blogItem.image}.jpg`)}
 					alt="not found"
 				/>
-			</div>
+			</div> */}
+{/* <BlogInfo /> */}
+			
+			{items}
 		</div>
 	);
 }
