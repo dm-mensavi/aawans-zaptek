af83a8a4812182a8418f65321df33cab5052c909
darkocobbygh <darkogaucho@gmail.com>
2023-11-04 12:16:55 +0000

    just finished attorneys page

--- src/Pages/Attorney.js
+++ src/Pages/Attorney.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import { Helmet } from "react-helmet";
+import { attorneyData } from './AttorneyData';
 
 function Attorney() {
   return (
@@ -9,6 +10,17 @@ function Attorney() {
 				<title>Attorney - AawAans</title>
 			</Helmet>
       <Header pageTitle="Our Attorney" name="Attorney" />
+     <div>
+         <div className='grid lg:grid-cols-4 sm:grid-cols-3 lg:gap-10 gap-8 rounded-xl lg:pt-40 pt-20 pb-20 p-10 lg:p-10 lg:pb-40'>
+        {attorneyData.map((attorneys)=>(
+          <div className='border-2 lg:p-6 p-6 rounded-lg flex flex-col items-center'>
+            <img src={attorneys.pic} alt='attorney pictures' />
+            <p className='font-bold font-serif text-2xl pt-4'>{attorneys.name} </p>
+            <p className='text-lg text-secondaryLight pt-2'>{attorneys.position} </p>
+          </div>
+        ))}
+      </div>
+     </div>
     </div>
   )
 }
